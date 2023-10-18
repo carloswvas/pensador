91d6fe28fd638e3b8fa370ec4368a88b8bf71137
Carlos Vasconcelos <81450652+carloswvas@users.noreply.github.com>
2023-10-18 08:02:01 -0300

    Add files via upload

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -1,9 +1,51 @@
-const Tought = require('../models/Tought')
-const User = require('../models/User')
+const Tought = require("../models/Tought");
+const User = require("../models/User");
 
-module.exports = class ToughtController{
-  static async showToughts(request, response){
-    return response.render('toughts/home')//Mostrando um view
+module.exports = class ToughtController {
+  static async showToughts(request, response) {
+    return response.render("toughts/home"); //Mostrando um view
   }
-}
 
+  static async dashboard(request, response) {
+    const userId = request.session.userId;
+
+    const user = await User.findOne({
+      where: {
+        id: userId,
+      },
+      include: Tought,
+      plain: true,
+    });
+
+    if (!user) {
+      response.redirect("/login");
+    }
+
+    const toughts = user.Toughts.map((result) => result.dataValues);
+    // console.log(toughts);
+
+    return response.render("toughts/dashboard", {toughts});
+  }
+
+  static createTought(request, response) {
+    return response.render("toughts/create");
+  }
+
+  static async createToughtSave(request, response) {
+    const tought = {
+      title: request.body.title,
+      UserId: request.session.userId,
+    };
+
+    try {
+      await Tought.create(tought);
+      request.flash("message", "Pensamento criado com sucesso!");
+
+      request.session.save(() => {
+        response.redirect("/toughts/dashboard");
+      });
+    } catch (error) {
+      console.log(`Aconteceu um erro: ${error}`);
+    }
+  }
+};
--- routes/toughtsRouters.js
+++ routes/toughtsRouters.js
@@ -4,6 +4,12 @@ const router = express.Router()
 //importar o controlador de pensamentos tought
 const ToughtController = require('../controllers/ToughtController')
 
+// importar o middleware de autenticação de usuário
+const checkAuth = require('../helpers/auth').checkAuth
+
+router.get('/dashboard', checkAuth, ToughtController.dashboard)
+router.get('/add', checkAuth, ToughtController.createTought)
+router.post('/add', checkAuth, ToughtController.createToughtSave)
 router.get('/', ToughtController.showToughts)
 
 //Exportar a rota
